ea5ad0c94a0eb168ce822468d0204a6ddcbdfbcf
mrcodink <shawnviera741@gmail.com>
2026-04-17 19:53:32 -0400

    wk13

---
+++ wk13/getusers_1.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Get Users</title>
+</head>
+<body>
+
+<form method="GET">
+    <input type="text" name="name" placeholder="Enter first name">
+    <button type="submit">Search</button>
+</form>
+
+<br>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Active</th>
+    </tr>
+
+
+</table>
+
+</body>
